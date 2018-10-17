85942bd6e824d56032bdfc7c07c6e21f5b68d7d2
Unknown <haskellt8@students.rowan.edu>
2018-10-16 21:34:34 -0400

    Complete and tested

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -21,9 +21,9 @@ void LEDSetup(void)
 void PWMSetup(void)
 {
     TA0CCR0 = 0xFF; // reset register
-    TA0CCR1 = 0x00; // red
-    TA0CCR2 = 0x00; // green
-    TA0CCR3 = 0x00; // blue
+    TA0CCR1 = 0xFF; // red
+    TA0CCR2 = 0xFF; // green
+    TA0CCR3 = 0xFF; // blue
 
     TA0CCTL1 = OUTMOD_7; // set/reset mode
     TA0CCTL2 = OUTMOD_7;
@@ -53,43 +53,56 @@ int main(void)
     __bis_SR_register(LPM0_bits + GIE); // low power mode + enable global interrupt
 }
 
-volatile int byteCount = 0;
-volatile int byteLn = 0;
+int byteCount = 0;
+int byteLn = 0;
 #pragma vector = USCI_A0_VECTOR
 __interrupt void USCI_A0 (void)
 {
     P1OUT |= BIT0; // status on
     // If there is data (i.e. not Null)
-    if(__even_in_range(UCA0IV, USCI_UCTXIFG) == USCI_UCRXIFG) // UCA0IV == USCI_UCRXIFG
+    switch(__even_in_range(UCA0IV, USCI_UCTXIFG)) // UCA0IV == USCI_UCRXIFG
     {
-        switch(byteCount)
-        {
-            case 0:
-                while(!(UCA0IFG & UCTXIFG)); // wait while transmitting
-                byteLn = UCA0TXBUF; // byte length
-                UCA0TXBUF = UCA0RXBUF - 0x03; // transmit all but first 3 bytes
-                break;
-            case 1:
-                TA0CCR1 = (UCA0RXBUF); // red
-                break;
-            case 2:
-                TA0CCR2 = (UCA0RXBUF); // green
-                break;
-            case 3:
-                TA0CCR3 = (UCA0RXBUF); // blue
-                break;
-            default:
-                while(!(UCA0IFG & UCTXIFG));
-                UCA0TXBUF = UCA0RXBUF; //Just transmit the incoming byte on to the next board
-        }
-        if(UCA0RXBUF == byteLn) // reset counter if last byte
-        {
-            byteCount = 1;
-        }
-        else
-        {
-            byteCount += 1;
-        }
+        case USCI_NONE:
+            break;
+
+        case USCI_UCRXIFG:
+            switch(byteCount)
+            {
+                case 0:
+                    while(!(UCA0IFG & UCTXIFG)); // wait while transmitting
+                    byteLn = UCA0TXBUF; // byte length
+                    UCA0TXBUF = UCA0RXBUF - 0x03; // transmit all but first 3 bytes
+                    __no_operation();
+                    break;
+                case 1:
+                    TA0CCR1 = (UCA0RXBUF); // red
+                    break;
+                case 2:
+                    TA0CCR2 = (UCA0RXBUF); // green
+                    break;
+                case 3:
+                    TA0CCR3 = (UCA0RXBUF); // blue
+                    break;
+                default:
+                    while(!(UCA0IFG & UCTXIFG));
+                    UCA0TXBUF = UCA0RXBUF; //Just transmit the incoming byte on to the next board
+            }
+            if(UCA0RXBUF == byteLn) // reset counter if last byte
+            {
+                byteCount = 1;
+            }
+            else
+            {
+                byteCount += 1;
+            }
+            break;
+
+        case USCI_UCTXIFG :
+            break;
+
+        default:
+            break;
+
     }
     P1OUT &= ~BIT0; // status off
 }
